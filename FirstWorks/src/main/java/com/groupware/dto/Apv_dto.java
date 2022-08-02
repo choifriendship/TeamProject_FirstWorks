3612149e6cbfa884c08788fd2cb2a99e04509409
choiwoojung <101532396+choiwoojung@users.noreply.github.com>
2022-08-02 21:07:13 +0900

    수정

--- FirstWorks/src/main/java/com/groupware/dto/Apv_dto.java
+++ FirstWorks/src/main/java/com/groupware/dto/Apv_dto.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class Apv_dto {
-	private int apv_no;	// 문서번호
-	private int mem_no; // 결재요청자
-	private int apv_mid_cf; // 중간결재자
-	private int apv_fnl_cf; // 최종결재자
+	private int apv_no; // 문서번호
+	private int apv_mem_no1; // 기안자
+	private int apv_mem_no2; // 기안자2
+	private int apv_mem_no3; // 기안자3
 	private int apv_cf_no; // 결재완료여부
 }
